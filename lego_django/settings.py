5334ebb8dcc9dfa0cd38db589ca3efbd978d3926
dirtywizzard <marcin.jakimiuk@gmail.com>
2022-06-12 20:46:59 +0200

    heroku deployment

--- lego_django/settings.py
+++ lego_django/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://legoartproject.herokuapp.com/']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
