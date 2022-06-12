3342ddc2ee2a73c6242bbaeb4500ae2c98772999
dirtywizzard <marcin.jakimiuk@gmail.com>
2022-06-12 19:48:07 +0200

    heroku deployment

--- lego_django/lego_django/settings.py
+++ lego_django/lego_django/settings.py
@@ -130,9 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_TMP = os.path.join(BASE_DIR, 'static')
 # STATIC_URL = '/static/'
@@ -143,6 +140,10 @@ os.makedirs(STATIC_ROOT, exist_ok=True)
 STATICFILES_DIRS = (
 os.path.join(BASE_DIR, 'static'),
 )
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
