d42c104dfd3b9cd70d9f20aeff75d553bf9557ce
dirtywizzard <marcin.jakimiuk@gmail.com>
2022-06-14 18:06:36 +0200

    heroku deployment

--- lego_django/settings.py
+++ lego_django/settings.py
@@ -93,6 +93,8 @@ DATABASES = {
         "NAME": BASE_DIR / "db.sqlite3",
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 
 # Password validation
