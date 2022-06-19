12700b6b6c78feaa4329e4fa0a3288e929f4573c
dirtywizzard <marcin.jakimiuk@gmail.com>
2022-06-19 15:21:49 +0200

    debug - false

--- lego_django/settings.py
+++ lego_django/settings.py
@@ -35,7 +35,7 @@ MEDIA_URL = "/media/"
 SECRET_KEY = os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com']
 
