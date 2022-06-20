804efc657b75073935b73902003a372d94272f72
dirtywizzard <marcin.jakimiuk@gmail.com>
2022-06-20 17:26:35 +0200

    settings.py update

--- lego_django/settings.py
+++ lego_django/settings.py
@@ -37,7 +37,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com']
+ALLOWED_HOSTS = ['.herokuapp.com','.ns.cloudflare.com']
 
 
 # Application definition
